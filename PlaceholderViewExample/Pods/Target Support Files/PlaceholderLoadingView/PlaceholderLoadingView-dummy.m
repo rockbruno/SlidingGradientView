df4e6700130724dbeed43660c5ef8f2e5284b75e
bruno-rocha-movile <bruno.rocha@movile.com>
2017-05-14 11:36:54 -0300

    Example pod

---
+++ PlaceholderViewExample/Pods/Target Support Files/PlaceholderLoadingView/PlaceholderLoadingView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PlaceholderLoadingView : NSObject
+@end
+@implementation PodsDummy_PlaceholderLoadingView
+@end
---
+++ PlaceholderViewExample/Pods/Target Support Files/Pods-PlaceholderViewExample/Pods-PlaceholderViewExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PlaceholderViewExample : NSObject
+@end
+@implementation PodsDummy_Pods_PlaceholderViewExample
+@end
